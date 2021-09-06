6b453c30fd0bba073cfb60d08128442921d8a3ce
wlacey <42483621+wlacey@users.noreply.github.com>
2021-09-06 15:48:13 +0900

    Update Q_18.py

--- Chapter 3 Exercise/Q_18.py
+++ Chapter 3 Exercise/Q_18.py
@@ -25,3 +25,29 @@ else:
       Penny = number // 1
 
 print("Quater = ",Quater, "Dimes = ", Dimes, "Nickel = ", Nickel, "Penny = ", Penny)
+
+# The following is a different approach...
+pennies = 0
+nickels = 0
+dimes = 0
+quarters = 0
+change = eval(input('Enter the amount of change as a whole number: '))
+while 1 > change or 99 < change:
+    change = (input('Enter the amount of change as a whole number: '))
+    continue
+for i in range(change):
+    pennies+=1
+    if pennies == 5:
+        pennies = 0
+        nickels+=1
+    if nickels == 2:
+        nickels = 0
+        dimes+=1
+    if dimes == 2 and nickels == 1:
+        dimes = 0
+        nickels = 0
+        quarters+=1
+cents = round(change/100, 2)
+print(f'Your change was {cents}. This will include {pennies} pennies, {nickels} nickels, {dimes} dimes, and {quarters} quarters.')
+# Range can only use integers, not floats.
+# So either take in integers or install numPy and import numpy module and call the arrange function.
