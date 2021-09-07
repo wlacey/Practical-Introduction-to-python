3fe9dfc186fef609da82074f2168e2494c74f30b
wlacey <42483621+wlacey@users.noreply.github.com>
2021-09-07 16:07:03 +0900

    Update Q_9.py

--- Chapter 4 Exercise/Q_9.py
+++ Chapter 4 Exercise/Q_9.py
@@ -7,3 +7,10 @@ number = eval(input("Enter a number: "))
 for i in range(1,number):
     if number % i == 0:
         print( i)
+        
+# I believe the final divisor is missing for above...
+num = int(input('Enter a number: '))
+print(f'The following are divisible by {num}.')
+for i in range(num):
+    if num % (i+1) == 0:
+        print(i+1)
