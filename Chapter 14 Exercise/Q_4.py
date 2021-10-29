696411165be00ce858e5d3ab700b698e67123bb7
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-29 13:31:25 +0900

    Update Q_4.py
    I believe the class I created is a more efficient approach to the problem since it utilizes the timedelta() function in the datetime module.

--- Chapter 14 Exercise/Q_4.py
+++ Chapter 14 Exercise/Q_4.py
@@ -6,46 +6,39 @@ formatted analogously to the previous method."""
 
 #NoteThe question has a slight mistake the Input of 230s should give an output of 3:50s and not 5:50s
 
-class Time:
+from datetime import timedelta
 
-    def __init__(self,seconds):
-        self.seconds = seconds
+class Time():
+    
+    def __init__(self, sec):
+        self.sec = sec
+        
+    def convert(self):
+        return timedelta(seconds = self.sec)
+    
+    def __str__(self):
+        return f'The current amount of seconds is {self.sec}.'
 
-    def convert_to_minutes(self):
-        if self.seconds < 60:
-           return self.seconds
-        else:
-           minute = int(self.seconds/60)
-           sec = self.seconds% 60
-           return "{} minutes :{} seconds".format(minute,sec)
-
-    def convert_to_hours(self):
-        if self.seconds < 60:
-            hour =0
-            minute = 0
-            sec = self.seconds
-            return "{} Hour :{} minutes :{} seconds".format(hour,minute,sec)
-        elif self.seconds > 60 and self.seconds< 3600:
-            hour = 0
-            minute = int(self.seconds/60)
-            sec = self.seconds % 60
-            return "{} Hour :{} minutes :{} seconds".format(hour, minute, sec)
-        elif self.seconds >= 3600:
-            hour = int(self.seconds/3600)
-            if self.seconds - 3600 < 60:
-               minute =0
+def validate_sec(message):
+    valid = False
+    while not valid:
+        try:
+            user_input = int(input(message))
+            if user_input >= 0:
+                valid = True
             else:
-                minute = int((self.seconds - 3600) / 60)
-            sec = int(self.seconds-3600)%60
-            return "{} Hour :{} minutes :{} seconds".format(hour, minute, sec)
+                print('\nPlease enter the amount in seconds.')
+        except:
+            print('\nPlease enter a valid integer.')
+    return user_input
 
-
-
-
-
-
-
-
-a =Time(5000)
-print(a.convert_to_minutes())
-print(a.convert_to_hours())
+def main():
+    print('Welcome to Time Conversion.')
+    message = 'Enter the number of seconds you want to convert: '
+    user_sec = validate_sec(message)
+    user = Time(user_sec)
+    print(f'\n{user}\n')
+    print(f'The converted time is {user.convert()}')
+    
+if __name__ == '__main__':
+    main()
