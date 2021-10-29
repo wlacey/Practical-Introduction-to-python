2db32b8f9ae483be9b5cdd583626c559ac4bad41
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-29 13:30:30 +0900

    Update Q_4.py
    I believe the class I created is a more efficient approach to the problem since it utilizes the timedelta() function in the datetime module.

--- Chapter 14 Exercise/Q_4.py
+++ Chapter 14 Exercise/Q_4.py
@@ -6,6 +6,7 @@ formatted analogously to the previous method."""
 
 #NoteThe question has a slight mistake the Input of 230s should give an output of 3:50s and not 5:50s
 
+"""
 class Time:
 
     def __init__(self,seconds):
@@ -49,3 +50,43 @@ class Time:
 a =Time(5000)
 print(a.convert_to_minutes())
 print(a.convert_to_hours())
+"""
+
+# I believe below is a more efficient approach to the problem since it utilizes the timedelta() function in the datetime module.
+
+from datetime import timedelta
+
+class Time():
+    
+    def __init__(self, sec):
+        self.sec = sec
+        
+    def convert(self):
+        return timedelta(seconds = self.sec)
+    
+    def __str__(self):
+        return f'The current amount of seconds is {self.sec}.'
+
+def validate_sec(message):
+    valid = False
+    while not valid:
+        try:
+            user_input = int(input(message))
+            if user_input >= 0:
+                valid = True
+            else:
+                print('\nPlease enter the amount in seconds.')
+        except:
+            print('\nPlease enter a valid integer.')
+    return user_input
+
+def main():
+    print('Welcome to Time Conversion.')
+    message = 'Enter the number of seconds you want to convert: '
+    user_sec = validate_sec(message)
+    user = Time(user_sec)
+    print(f'\n{user}\n')
+    print(f'The converted time is {user.convert()}')
+    
+if __name__ == '__main__':
+    main()
